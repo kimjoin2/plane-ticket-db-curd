d0c27ccfc34505115a847c509b7867f9e6d968ef
KIM BUMNYEONG <kimjoin2@gmail.com>
2019-04-27 10:52:57 +0900

    Add file and fix time to string format

--- utils/timeToString.go
+++ utils/timeToString.go
@@ -7,6 +7,6 @@ import (
 
 func TimeToStringUntilMin(t time.Time) string {
 	res := strconv.Itoa(t.Year()) + "/" + strconv.Itoa(int(t.Month())) + "/"
-	res += strconv.Itoa(t.Day()) + "/" + strconv.Itoa(t.Hour()) + ":" + strconv.Itoa(t.Minute())
+	res += strconv.Itoa(t.Day()) + " " + strconv.Itoa(t.Hour()) + ":" + strconv.Itoa(t.Minute())
 	return res
 }
