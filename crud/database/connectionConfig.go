9a083f867ae7f18829fe720c19eef895bf901203
KIM BUMNYEONG <kimjoin2@gmail.com>
2019-04-29 12:06:23 +0900

    Fix UTC timezone for database

--- crud/database/connectionConfig.go
+++ crud/database/connectionConfig.go
@@ -1,7 +1,7 @@
 package database
 
 func getSourcePath() string {
-	const sourcePath = "root:1q2w3e4r@tcp(127.0.0.1:3306)/plane_ticket?parseTime=true"
+	const sourcePath = "root:1q2w3e4r@tcp(127.0.0.1:3306)/plane_ticket?parseTime=true&loc=Local"
 	return sourcePath
 }
 
